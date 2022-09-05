41756f1f774ee97b15afacd54ed8ee1a0338daf8
haidarghazi <112367358+haidarghazi@users.noreply.github.com>
2022-09-04 22:55:02 -0700

    Add files via upload

---
+++ python6-tkj3.py
@@ -0,0 +1,44 @@
+# dolar ke rupiah
+
+USD = int(input("masukan nilai dolar : "))
+IDR = USD * 14916.50
+
+print("nilai rupiah adalah", IDR)
+
+# euro ke won
+
+EUR = int(input("masukan nilai euro : "))
+KRW = EUR * 1356.10
+
+print("nlai won adalah", KRW)
+
+# yen ke rupiah
+
+JPY = int(input("masukan nilai yen : "))
+IDR = JPY * 106.29
+
+print("nilai rupiah adalah", IDR)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
