ebef203f940d793c754352767db0af6188851b5d
haidarghazi <112367358+haidarghazi@users.noreply.github.com>
2022-09-08 00:02:32 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,44 @@
+# rumus persegi panjang p * l
+
+print("\n1.luas persegi panjang")
+p = int(input("masukkan nilai p: "))
+l = int(input("masukkan nilai l: "))
+luas_pp = p * l
+
+print("luas persegi panjang adalah", luas_pp)
+
+# rumus belah keupat 1/2 * d1 * d2
+
+print("\n2.luas belah ketupat")
+d1 = int(input("masukkan nilai d1: "))
+d2 = int(input("masukkan nilai d2: "))
+luas_bk = 1/2 * d1 * d2
+
+print("luas belah ketupat adalah", luas_bk)
+
+#rumus segitiga a * t / 2
+
+print("\n3.luas segitiga")
+a = int(input("masukkan nilai a: "))
+t = int(input("masukkan nilai t: "))
+luas_s = a * t / 2
+
+print("luas segitiga adalah", luas_s)
+
+# rumus lingkaran 22/7 * r * r
+
+print("\n4.luas lingkaran")
+r = int(input("masukan nilai r: "))
+luas_l = 22/7 * r * r
+
+print("luas lingkaran adalah", luas_l)
+
+# rumus persegi sisi * sisi
+
+print("\n5.luas persegi")
+sisi = int(input("masuakan nilai sisi: "))
+luas_P = sisi * sisi
+
+print("luas persegi adalah", luas_P)
+    
+
